91e6652470b9ff89723573e8e558a999490dd28b
Kevinoh47 <kohok47@gmail.com>
2018-08-22 20:37:30 -0700

    first two requirements basically working

---
+++ server.js
@@ -0,0 +1,36 @@
+require ('dotenv').config();
+const pg = require('pg');
+const express = require('express');
+const PORT = process.env.PORT;
+const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('./public'));
+
+app.get('/ping', (request, response) => {
+  response.send('pong');
+});
+app.get('/hello', (request, response) => {
+  console.log('inside hello route...');
+  //render index.ejs
+  response.render('index');
+});
+app.get('/books', (request, response) => {
+  client.query(`SELECT title, author, image_url FROM books ORDER BY title;`)
+    // .then(result => { response.send(result.rows); })
+    .then(results => response.render('index', {books : results.rows}))
+    .catch(err => {
+      console.error(err);
+      response.status(500).send(err);
+    });
+});
+
+//404
+app.use('*', (request, response) => {response.render('pages/error');});
+
+app.listen(PORT, () => console.log('listening on PORT',PORT));
+
+
