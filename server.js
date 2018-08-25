3f0e6323fc7f57761a711396f6b4c2ce920f6eae
Kevinoh47 <kohok47@gmail.com>
2018-08-25 12:14:10 -0700

    added book specific route and ejs template

--- server.js
+++ server.js
@@ -23,10 +23,22 @@ app.get('/ping', (request, response) => {
 
 //books list route.
 app.get('/books', (request, response) => {
-  client.query(`SELECT title, author, image_url FROM books ORDER BY title;`)
+  client.query(`SELECT image_url, title, author, id FROM books ORDER BY title;`)
     .then(results => response.render('index', {books : results.rows}))
     .catch(err => {
       console.error(err);
+      response.status(500).send(err);
+    });
+});
+
+app.get('/books/:id', (request, response) => {
+  let sql = `SELECT image_url, title, author, isbn, description FROM books WHERE id = $1`;
+  let values = [request.params.id];
+  client.query(sql, values)
+    .then(
+      results => response.render('pages/show', {books : results.rows}))
+    .catch(err => {
+      console.log(err);
       response.status(500).send(err);
     });
 });
